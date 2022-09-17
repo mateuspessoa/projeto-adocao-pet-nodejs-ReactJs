3343a77ddaa3aad7fcdf7c2bf48f3e31251e6054
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-17 00:31:47 -0300

    Criando Validações dos dados principais e se o usuario existe

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,7 +3,39 @@ const User = require('../models/User')
 module.exports = class UserController {
 
     static async register(req, res) {
-        res.json('Olá Get a Pet')
+
+        const {name, email, phone, password, confirmpassword} = req.body
+
+        //Validações
+        if(!name) {
+            res.status(422).json({ message: 'O nome é obrigatório' })
+            return
+        }
+
+        if(!email) {
+            res.status(422).json({ message: 'O email é obrigatório' })
+            return
+        }
+
+        if(!phone) {
+            res.status(422).json({ message: 'O telefone é obrigatório' })
+            return
+        }
+
+        if(!password) {
+            res.status(422).json({ message: 'A senha é obrigatório' })
+            return
+        }
+
+        if(!confirmpassword) {
+            res.status(422).json({ message: 'A confirmação da senha é obrigatório' })
+            return
+        }
+
+        if(password !== confirmpassword) {
+            res.status(422).json({ message: 'As senhas precisam ser iguais' })
+            return
+        }
     }
 
 }
