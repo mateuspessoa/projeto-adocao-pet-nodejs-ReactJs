0187db8e9acb7a69234969ce1ad1ddb88e9e8f81
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-18 00:45:18 -0300

    Resgatando o usuário pelo ID

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -6,5 +6,6 @@ router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 
 router.get('/checkUser', UserController.checkUser)
+router.get('/:id', UserController.getUserById)
 
 module.exports = router
