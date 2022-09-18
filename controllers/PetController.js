39b4dee66848ec0e827a0fa5e4e01d48c0409ce9
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-18 19:12:37 -0300

    Função de criação de pet e salvamento do pet no banco de dados

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -1,10 +1,73 @@
 const Pet = require('../models/Pet')
 
+//Helpers
+const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
+
 module.exports = class PetController {
 
     //Criar um pet
     static async create(req, res) {
-        res.json({message: 'Deu certo!'})
+        
+        const {name, age, weight, color} = req.body
+
+        const available = true
+
+        //Upload de imagens
+
+        //Validações
+        if(!name) {
+            res.status(422).json({message: "O nome do pet é obrigatório"})
+            return
+        }
+
+        if(!age) {
+            res.status(422).json({message: "A idade do pet é obrigatória"})
+            return
+        }
+
+        if(!weight) {
+            res.status(422).json({message: "O peso do pet é obrigatório"})
+            return
+        }
+
+        if(!color) {
+            res.status(422).json({message: "A cor do pet é obrigatória"})
+            return
+        }
+
+        //Pegando o usuário dono do pet para poder adicionar ao pet
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        //Criando um pet
+        const pet = new Pet({
+            name,
+            age,
+            weight,
+            color,
+            available,
+            images: [],
+            user: {
+                _id: user._id,
+                name: user.name,
+                image: user.image,
+                phone: user.phone,
+            },
+        })
+
+        //Salvar o pet no banco de dados
+        try {
+
+            const newPet = await pet.save()
+            res.status(201).json({
+                message: "Pet cadastrado com sucesso",
+                newPet,
+            })
+
+        } catch(error) {
+            res.status(500).json({message: error})
+        }
     }
 
 }
--- routes/PetRoutes.js
+++ routes/PetRoutes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 const PetController = require('../controllers/PetController')
 
+//Middlewares
+//Apenas usuários autenticados poderão adicionar pets
+const verifyToken = require('../helpers/verify-token')
 
-router.post('/create', PetController.create)
+
+router.post('/create', verifyToken, PetController.create)
 
 module.exports = router
